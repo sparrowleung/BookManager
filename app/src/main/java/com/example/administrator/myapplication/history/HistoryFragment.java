f4b3ba1d6681d646eff4a38f3f3bf68667b4a5a3
sparrowleung <372895070@qq.com>
2017-11-19 23:46:24 +0800

    Finish History+News

--- app/src/main/java/com/example/administrator/myapplication/history/HistoryFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/history/HistoryFragment.java
@@ -1,12 +1,22 @@
 package com.example.administrator.myapplication.history;
 
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.base.BaseFragment;
+import com.example.administrator.myapplication.recycleview.book;
+import com.example.administrator.myapplication.recycleview.bookAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 37289 on 2017/11/15.
@@ -15,6 +25,16 @@ import com.example.administrator.myapplication.base.BaseFragment;
 public class HistoryFragment extends BaseFragment {
 
     private View _rootView;
+
+    private List<book> mList;
+    private RecyclerView mRecyclerView;
+    private bookAdapter mAdapter;
+
+    private CardView mCard1;
+    private CardView mCard2;
+    private ImageView mImageView;
+    private TextView mName;
+    private TextView mBookNumber;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle saveInstanceState){
@@ -26,5 +46,25 @@ public class HistoryFragment extends BaseFragment {
     @Override
     public void onActivityCreated(Bundle saveInstanceState){
         super.onActivityCreated(saveInstanceState);
+
+        mRecyclerView=(RecyclerView) getActivity().findViewById(R.id.recyclerview_history);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mList=new ArrayList<>();
+        for(int i=0;i<6;i++){
+            book b1=new book("a",R.mipmap.ic_launcher);
+            mList.add(b1);
+            book b2=new book("b",R.mipmap.ic_launcher);
+            mList.add(b2);
+            book b3=new book("c",R.mipmap.ic_launcher);
+            mList.add(b3);
+        }
+        mAdapter=new bookAdapter(mList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mCard1=(CardView) getActivity().findViewById(R.id.history_card1);
+        mCard2=(CardView) getActivity().findViewById(R.id.history_card2);
+        mImageView=(ImageView) getActivity().findViewById(R.id.hotbook_image);
+        mName=(TextView) getActivity().findViewById(R.id.history_account);
+        mBookNumber=(TextView) getActivity().findViewById(R.id.history_lendcount);
     }
 }
--- app/src/main/java/com/example/administrator/myapplication/newsandtips/NewsAndTipsFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/newsandtips/NewsAndTipsFragment.java
@@ -1,12 +1,20 @@
 package com.example.administrator.myapplication.newsandtips;
 
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.base.BaseFragment;
+import com.example.administrator.myapplication.recycleview.book;
+import com.example.administrator.myapplication.recycleview.bookAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 37289 on 2017/11/15.
@@ -15,6 +23,11 @@ import com.example.administrator.myapplication.base.BaseFragment;
 public class NewsAndTipsFragment extends BaseFragment {
 
     private View _rootView;
+
+    private List<book> mList;
+    private RecyclerView mRecyclerView;
+    private bookAdapter mAdapter;
+    private CardView mCard;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle saveInstanceState){
@@ -26,5 +39,21 @@ public class NewsAndTipsFragment extends BaseFragment {
     @Override
     public void onActivityCreated(Bundle saveInstanceState){
         super.onActivityCreated(saveInstanceState);
+
+        mRecyclerView=(RecyclerView) getActivity().findViewById(R.id.recyclerview_news);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mList=new ArrayList<>();
+        for(int i=0;i<6;i++){
+            book b1=new book("a",R.mipmap.ic_launcher);
+            mList.add(b1);
+            book b2=new book("b",R.mipmap.ic_launcher);
+            mList.add(b2);
+            book b3=new book("c",R.mipmap.ic_launcher);
+            mList.add(b3);
+        }
+        mAdapter=new bookAdapter(mList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mCard=(CardView) getActivity().findViewById(R.id.news_card);
     }
 }
